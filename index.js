4ce1106b9ce7c74084f727f58a4ba178121d6a29
Flavien LAUREAU <flavien.laureau@gmail.com>
2021-03-14 00:38:24 +0100

    bug fix

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Discord.Client({
 // La clé "partials" est là pour autoriser le client à se servir des partials. Elle servent à récuperer le cache même si on relance le bot
 // La clé "ws" est là pour configurer le client discord. Pour le moment il me sert uniquement à pouvoir récuperer les "guilds.members.fetch()". Il faut penser également à allé activer les "intents", dans le panel "discord.com/developers".
 
+require("./util/functions")(client);
 client.config = require("./config");
 client.mongoose = require("./util/mongoose");
 client.commands = new Discord.Collection();
