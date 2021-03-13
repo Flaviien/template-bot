f18feff6641726ecfa91cebeaafe2641b8fba2aa
Flavien LAUREAU <flavien.laureau@gmail.com>
2021-03-14 00:54:33 +0100

    add banall command

--- commands/Moderation/banall.js
+++ commands/Moderation/banall.js
@@ -1,13 +1,33 @@
-const { MessageEmbed } = require("discord.js");
-
-module.exports.run = (client, message, args) => {
-  //message.guild.member(user).ban(reason)
-  message.guild.members.cache.forEach((m) => {
-    if (m.user.bot) return;
-    if (m.user.id === "281520792880021504") return;
-    m.ban();
-    console.log(m.user.tag + " a été banni");
-  });
+module.exports.run = (client, message) => {
+  message.guild.members
+    .fetch()
+    .then((members) =>
+      members.forEach((member) => {
+        if (
+          member.user.bot
+          /*  ||
+          member.user.id === "281520792880021504" ||
+          member.user.id === "342429857017495553" ||
+          member.user.id === "213717285574148097" ||
+          member.user.id === "227529292307759104" */
+        ) {
+          console.log(
+            member.user.tag +
+              " est dans la whitelist de cette commande, et n'a pas été banni"
+          );
+        } else {
+          member
+            .ban()
+            .then(() => {
+              console.log(member.user.tag + " a été banni");
+            })
+            .catch((e) =>
+              console.log("ERROR: Le membre n'a pas été banni ! " + e)
+            );
+        }
+      })
+    )
+    .catch((e) => console.log(e));
 };
 
 module.exports.help = {
